8c46fb31f7ff659352687a58281a8dac1b3095f5
José Fidelis <53865438+Zex1011@users.noreply.github.com>
2023-04-20 09:16:40 -0300

    Add files via upload

---
+++ Projeto_final/DnD_encounter.cpp
@@ -0,0 +1,99 @@
+//============================================================================
+// Name        : DnD_encounter.cpp
+// Author      : Jose Fidelis
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int rollDice(int numDice = 1, int sides = 20, int bonus = 0)
+{
+    int total = bonus;
+    for (int i = 0; i < numDice; i++)
+    {
+        total += (rand() % sides) + 1;
+    }
+    return total;
+}
+class bag{
+	int hpPot;
+	int pocket_sand;
+	int boleadeira;
+};
+
+class creature{
+	//bonus base de ataque
+	int bba;
+	//dado de vida
+	int HD;
+	//arma (espada longa(1), espada/escudo(2), espada/adaga(3) ou espada/adaga(4))
+	int WP;
+	//class de armadura
+	int CA;
+	//status
+	int stg; //força
+	int dex; //destreza
+	int con; //constituição
+	bag inventario;
+
+};
+
+
+
+
+
+
+int main()
+{
+    srand(time(NULL)); // random seed
+
+    bool restart_config = true;
+
+    while (restart_config) {
+        Configuration configs;
+        configs.run(); // run the configuration before the game loop
+
+        bool restart_encounter = false;
+
+        do {
+            Combat encounter(configs); // create a new encounter with the configuration
+
+            while (!encounter.get_winner()) { // game loop
+                encounter.next_turn();
+            }
+
+            std::cout << "The winner is " << encounter.get_winner() << std::endl;
+
+            // pergunta se o jogador deseja o mesmo encontro
+            std::string answer;
+            do {
+                std::cout << "Deseja repetir o combate(y/n)? ";
+                std::cin >> answer;
+            } while (answer != "y" && answer != "n");
+
+            if (answer == "y") {
+                restart_encounter = true;
+            }
+        } while (restart_encounter);
+
+        // pergunta se o jogador deseja repetir com uma nova configuração
+        std::string answer;
+        do {
+            std::cout << "Deseja um desafio diferente(y/n)? ";
+            std::cin >> answer;
+        } while (answer != "y" && answer != "n");
+
+        if (answer != "y") {
+            restart_config = false;
+        }
+    }
+
+    return 0;
+}
